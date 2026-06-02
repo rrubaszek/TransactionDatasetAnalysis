423f27b5b6f3f69211050b389f2a5de5f9e3ddf6
rrubaszek <robert.rubaszek@gmail.com>
2026-06-02 22:39:40 +0200

    fixed requested changes

--- src/transaction_analysis/models/step/run.py
+++ src/transaction_analysis/models/step/run.py
@@ -7,7 +7,7 @@ from sklearn.utils import resample
 from transaction_analysis.config.logger import setup_logging
 from transaction_analysis.config.paths import TRAINED_MODELS_DIR
 from transaction_analysis.models.compare import compare_models
-from transaction_analysis.models.config import RFConfig, XGBConfig
+from transaction_analysis.models.config import CrossValidationConfig, RFConfig, XGBConfig
 from transaction_analysis.models.evaluate import print_report, summarise_cv
 from transaction_analysis.models.factory import ModelType, cross_validate_model, train_model
 from transaction_analysis.models.features import build_features
@@ -22,8 +22,12 @@ def run(force_cross_validate: bool = False, use_resample: bool = True):
         X_cv, y_cv = resample(X, y, n_samples=2_000_000, stratify=y, random_state=42)
 
     if force_cross_validate:
-        xgb_results = cross_validate_model(ModelType.XGB, XGBConfig(max_depth=5), X_cv.values, y_cv.values)
-        rf_results = cross_validate_model(ModelType.RF, RFConfig(max_depth=5), X_cv.values, y_cv.values)
+        xgb_results = cross_validate_model(
+            ModelType.XGB, XGBConfig(max_depth=5), CrossValidationConfig(), X_cv.values, y_cv.values
+        )
+        rf_results = cross_validate_model(
+            ModelType.RF, RFConfig(max_depth=5), CrossValidationConfig(), X_cv.values, y_cv.values
+        )
 
         logger.info(summarise_cv(xgb_results).to_string())
         print_report(xgb_results, ModelType.XGB)
